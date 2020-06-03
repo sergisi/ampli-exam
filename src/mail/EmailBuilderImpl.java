466be2ca3a4ca9dee940c8d3f6df3cb37e8ae85c
Sergi Simon Balcells <sergisiba@gmail.com>
2020-06-03 19:59:17 +0200

    Made package private

--- src/mail/EmailBuilderImpl.java
+++ src/mail/EmailBuilderImpl.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class EmailBuilderImpl implements EmailBuilder {
+class EmailBuilderImpl implements EmailBuilder {
 
     private String from, subject, body;
     private List<String> to, ccTo;
